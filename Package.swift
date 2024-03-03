5817e37678eb0f2ef6adf6cb4f9924dc068a7672
Vedant Jha <6vedant@gmail.com>
2024-03-03 22:46:12 +0530

    TaskManagementSDK class added with utility methods

--- Package.swift
+++ Package.swift
@@ -11,11 +11,18 @@ let package = Package(
             name: "TaskManagmentSDK",
             targets: ["TaskManagmentSDK"]),
     ],
+    dependencies: [
+            .package(url: "https://github.com/OpenCombine/OpenCombine.git", from: "0.10.0")
+        ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
+        
         .target(
-            name: "TaskManagmentSDK"),
+            name: "TaskManagmentSDK",
+            dependencies: [
+                         .product(name: "OpenCombine", package: "OpenCombine"),
+                        ]),
         .testTarget(
             name: "TaskManagmentSDKTests",
             dependencies: ["TaskManagmentSDK"]),
--- Sources/TaskManagmentSDK/TaskManagmentSDK.swift
+++ Sources/TaskManagmentSDK/TaskManagmentSDK.swift
@@ -1,2 +1,57 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import Dispatch
+import Foundation
+import OpenCombine
+
+
+// Model for a task
+struct Task: Identifiable {
+  let id: Int
+  let title: String
+}
+
+// ViewModel to manage tasks
+class TaskManagmentSDK {
+  static let viewModel = TaskManagmentSDK()
+  private init() {}
+
+  var tasks: [Task] = [] {
+    didSet {
+      tasksPublisher.send(tasks)
+    }
+  }
+
+  let tasksPublisher = CurrentValueSubject<[Task], Never>([])
+
+  private var cancellables: [AnyCancellable] = [] // Use non-optional array
+
+
+  func addTask(_ task: Task) {
+    tasks.append(task)
+    print("Task added: \(task.title)")
+  }
+
+  func removeTask(_ task: Task) {
+    tasks.remove(at: task.id)
+    print("Task removed: \(task.title)")
+  }
+
+  func getTasksResultString() -> [String] {
+    var taskStringArray = [String]()
+    print("Current tasks:")
+    for task in tasks {
+      taskStringArray.append(task.title)
+    }
+    return taskStringArray
+  }
+
+ func subscribeToChanges(handler: @escaping ([Task]) -> Void) {
+         tasksPublisher
+             .sink { tasks in
+                 handler(tasks)
+             }
+             .store(in: &cancellables)
+ }
+}
